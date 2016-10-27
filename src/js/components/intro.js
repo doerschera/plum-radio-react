0be2c88a5f4d4a7e87774731153a7ea22ff6506b
doerschera <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-10-27 18:23:55 -0500

    style bugs

--- src/js/components/intro.js
+++ src/js/components/intro.js
@@ -5,7 +5,7 @@ export default class Intro extends React.Component {
   render() {
     return (
       <div>
-        <h2 class='title white-box' id='home-title'>Plum Radio</h2>
+        <h2 class='title white-box' id='homeTitle'>Plum Radio</h2>
         <h3 class='white-box lines'>a tool to help writers get tuned in</h3>
         <div>
           <p class='white-box'>random poem fragments provide a starting point for freewriting. gut mode is timed: 60 seconds to get words flowing. zen mode is untimed, take as long as you want. you can change mode any time in the top bar. click the "past lines" tab to see what you have written for each fragment.</p>
